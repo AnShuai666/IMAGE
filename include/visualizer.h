4bd4b5bdf9b5c612580fb03d1b1b8ee92340cf1a
AnShuai666 <1028792866@qq.com>
2019-04-01 15:53:00 +0800

    添加可视化函数

--- include/visualizer.h
+++ include/visualizer.h
@@ -9,6 +9,8 @@
 #define IMAGE_VISUALLIZER_H
 
 #include "define.h"
+#include <vector>
+
 IMAGE_NAMESPACE_BEGIN
 class Visualizer
 {
@@ -30,10 +32,30 @@ public:
     };
 
 public:
+    /*
+    *  @property   画特征点
+    *  @func       在灰度图像上画指定形状的特征点
+    *  @param_in   image        待绘图像
+    *  @param_in   keypoint     关键点
+    *  @param_in   style        关键点绘画风格:方框\圆形等等
+    *  @param_in   color        绘画颜色
+    *  @static     静态成员函数  属于类不属于对象的类成员函数
+    *  @return     void
+    */
     static void draw_keypoint(image::ByteImage& image, Keypoint const& keypoint,KeypointStytle style,uint8_t const* color);
 
+    /*
+    *  @property   画特征点
+    *  @func       在灰度图像上画指定形状的特征点
+    *  @param_in   image        待绘图像
+    *  @param_in   keypoint     关键点序列
+    *  @param_in   style        关键点绘画风格:方框\圆形等等
+    *  @static     静态成员函数  属于类不属于对象的类成员函数
+    *  @return     void
+    */
+    static image::ByteImage::Ptr draw_keypoints(image::ByteImage::ConstPtr image, std::vector<Keypoint> const& keypoints, KeypointStytle style);
 
-
+    static image::ByteImage::Ptr draw_keypoints(image::ByteImage::ConstPtr image1,image::ByteImage::ConstPtr image2);
 
 };
 
