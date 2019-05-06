28bd4a39c654573a753ad70b537b1df940d0e572
yft1996 <yangfengtuo@163.om>
2019-05-06 17:02:27 +0800

    add_channels函数实现及其模板化

--- include/cuda_include/image.cuh
+++ include/cuda_include/image.cuh
@@ -34,4 +34,27 @@ template <>
 int fill_color_by_cuda<float>(float  *image,float *color,int const w,int const h,int const c,int const color_size,float *contrast);
 
 
+/**
+ * @func            添加颜色通道
+ * @property        为已存在图像添加一个新的颜色通道
+ * @param_out       dst_image       添加颜色通道后的图像
+ * @param_in        src_image       已存在图像
+ * @param_in        w               图像宽
+ * @param_in        h               图像高
+ * @param_in        c               图像颜色通道
+ * @param_in        num_channels    所要添加的通道数
+ * @param_in        value           添加的颜色通道内所要赋的值
+ * @param_in        contrast        cpu实现,与gpu实现对比
+ * @return
+ * 调用示例：
+ * add_channels_by_cuda(&dst.at(0),&src1.at(0),src1.width(),src1.height(),src1.channels(),num_channels,value,&src.at(0));
+ */
+template <typename T>
+int add_channels_by_cuda(T *dst_image,T  * src_image,int const w,int const h, int const c, int const num_channels, T const value,T *contrast);
+template <>
+int add_channels_by_cuda<char>(char *dst_image,char  * src_image,int const w,int const h, int const c, int const num_channels,char const value,char *contrast);
+template <>
+int add_channels_by_cuda<float>(float *dst_image,float  * src_image,int const w,int const h, int const c, int const num_channels,
+                                float const value,float *contrast);
+
 #endif //_IMAGE_CUH_
--- src/cuda_src/image.cu
+++ src/cuda_src/image.cu
@@ -17,6 +17,17 @@ void gpu_cpu2zero1(T *cpu,T *gpu,size_t bytes)
     cudaMemset(gpu,0,bytes);
 }
 
+
+/*网格划分
+for ( x = 32; x >8 ; x>>=1)
+{
+    for ( y = 32; y >2 ; y>>=1)
+    {
+    if (x * y < 128)continue;
+    std::cout<<"block("<<x<<","<<y<<")"<<std::endl;
+    }
+}
+*/
 /******************************************************************************************/
 ///功能：填充图像
 /*  函数名                           线程块大小       耗费时间
@@ -25,7 +36,7 @@ void gpu_cpu2zero1(T *cpu,T *gpu,size_t bytes)
  *  kernel_fill_color3_by_share	    400.097us	    [32,4,1]
  *  kernel_fill_color15_by_share	253.638us	    [32,4,1]**
  */
-/******************************************************************************************/
+///核函数
 /* 调用示例
  * dim3 block(x,y,1);
  * dim3 grid((wc-1+x)/x,(h-1+y)/y,1);
@@ -137,7 +148,80 @@ __global__ void kernel_fill_color15_by_share(T * image, T *color,int const wc,in
     }
 }
 
-
+///功能:添加颜色通道
+/*  函数名                           线程块大小       耗费时间
+ *  kernel_add_channels	            1.131ms	        [32,4,1]
+ *  kernel_add_channels_stride	    507.197us	    [32,4,1]
+ *  kernel_add_channels_stride2	    422.649us	    [32,4,1]**
+ */
+///核函数
+/* 调用示例
+ * dim3 block(x,y,1);
+ * dim3 grid((w*c_add-1+x)/(x),(h-1+y)/y,1);
+ * kernel_add_channels<char><<<grid,block>>>(d_out,d_in,w,h,c,num_channels,value);
+ */
+template <typename T>
+__global__ void kernel_add_channels(T *dst,T *src, int const w,int const h,int const c,int const num_channels,T const value)
+{
+    int x=threadIdx.x+blockIdx.x*blockDim.x;//x坐标索引
+    int y=threadIdx.y+blockIdx.y*blockDim.y;//y坐标索引
+    int c_add=c+num_channels;
+    int idx=y*w*c_add+x;//输出索引
+    if(x<w*c_add&&y<h)
+    {
+        int channels=idx%c_add;
+        int pixels=idx/c_add;
+        if(channels<c) dst[idx]=src[pixels*c+channels];
+        else dst[idx]=value;
+    }
+}
+/* 调用示例
+ * dim3 block(x,y,1);
+ * dim3 grid((w-1+x)/(x),(h-1+y)/y,1);
+ * kernel_add_channels_stride<char><<<grid,block>>>(d_out,d_in,w,h,c,num_channels,value);
+ */
+template <typename T>
+__global__ void kernel_add_channels_stride(T *dst,T *src, int const w,int const h,int const c,int const num_channels,T const value)
+{
+    int x = threadIdx.x + blockIdx.x * blockDim.x;//x坐标索引
+    int y = threadIdx.y + blockIdx.y * blockDim.y;//y坐标索引
+    int c_add=c+num_channels;
+    int idx_out = y * w * c_add + x * c_add;//输出索引
+    int idx_in = y * w * c + x * c;//输入索引
+    if (x < w * c_add && y < h)
+    {
+        for (int i = 0; i < c; ++i) dst[idx_out + i] = src[idx_in + i];
+        for (int j = 0; j < num_channels; ++j) dst[idx_out + c + j] = value;
+    }
+}
+/* 调用示例
+ * dim3 block(x,y,1);
+ * dim3 grid((w-1+x*2)/(x*2),(h-1+y)/y,1);
+ * kernel_add_channels_stride2<char><<<grid,block>>>(d_out,d_in,w,h,c,num_channels,value);
+ */
+template <typename T>
+__global__ void kernel_add_channels_stride2(T *dst,T *src, int const w,int const h,int const c,int const num_channels,T const value)
+{
+    int x=threadIdx.x+blockIdx.x*blockDim.x*2;//x坐标索引
+    int y=threadIdx.y+blockIdx.y*blockDim.y;//y坐标索引
+    int c_add=c+num_channels;
+    int idx_out=y*w*c_add+x*c_add;//输出索引
+    int idx_in=y*w*c+x*c;//输入索引
+    T d_value=value;
+    if (x < w * c_add && y < h)
+    {
+        for (int i = 0; i <c ; ++i)
+        {
+            dst[idx_out+i]=src[idx_in+i];
+            dst[idx_out+blockDim.x*c_add+i]=src[idx_in+blockDim.x*c+i];
+        }
+        for (int j = 0; j <num_channels ; ++j)
+        {
+            dst[idx_out+c+j]=d_value;
+            dst[idx_out+blockDim.x*c_add+c+j]=d_value;
+        }
+    }
+}
 
 /******************************************************************************************/
 ///调用核函数实现加速功能
@@ -148,7 +232,7 @@ int fill_color_cu(T *image,T *color,int const w,int const h,int const c,int cons
     //bool flag= false;
     if(c!=color_size)
     {
-        std::cout<<"颜色通道不匹配"<<std::endl;
+        std::cerr<<"颜色通道不匹配"<<std::endl;
         return 0;
     }
     int wc=w*c;
@@ -199,4 +283,62 @@ int fill_color_by_cuda<float>(float  *image,float *color,int const w,int const h
     fill_color_cu<float>(image,color,w,h,c, color_size);
     //compare1<float>(image,contrast,w*c,h, true);
     return 0;
+}
+
+template <typename T>
+int add_channels_cu(T *dst_image,T * src_image,int const w,int const h, int const c, int const num_channels,T const value)
+{
+    if(num_channels<=0)
+    {
+        std::cerr<<"所添加的颜色通道个数小于1"<<std::endl;
+        return 0;
+    }
+    int const wc =w*c;//输入图像实际宽度
+    int const wc_add=w*(c+num_channels);//输出图像实际宽度
+    //计算存储空间字节数
+    size_t const bytes_src=wc*h* sizeof(T);
+    size_t const bytes_dst=wc_add*h* sizeof(T);
+    //声明显存指针
+    T *d_in,*d_out;
+    //定义显存指针
+    cudaMalloc((void**)&d_in,bytes_src);
+    cudaMalloc((void**)&d_out,bytes_dst);
+    //cpu2gpu
+    cudaMemcpy(d_in,src_image,bytes_src,cudaMemcpyHostToDevice);
+    //int c_add=c+num_channels;
+    //网格划分
+    int x=32;
+    int y=4;
+    dim3 block(x,y,1);
+    dim3 grid((w-1+x*2)/(x*2),(h-1+y)/y,1);
+
+    kernel_add_channels_stride2<T><<<grid,block>>>(d_out,d_in,w,h,c,num_channels,value);
+
+    //gpu2cpu
+    cudaMemcpy(dst_image,d_out,bytes_dst,cudaMemcpyDeviceToHost);
+    //compare1(dst_image,contrast,w*c,h, false);
+    ///释放显存指针
+    cudaFree(d_in);
+    cudaFree(d_out);
+    return 0;
+}
+template <typename T>
+int add_channels_by_cuda(T *dst_image,T  * src_image,int const w,int const h, int const c, int const num_channels,T const value,T *contrast)
+{
+    add_channels_cu(dst_image,src_image, w, h, c,num_channels,value);
+    return  0;
+}
+template <>
+int add_channels_by_cuda<char>(char *dst_image,char  * src_image,int const w,int const h, int const c, int const num_channels,char const value,char *contrast)
+{
+    add_channels_cu<char>(dst_image,src_image, w, h, c,num_channels,value);
+    //compare1(dst_image,contrast,w*c,h, false);
+    return  0;
+}
+template <>
+int add_channels_by_cuda<float>(float *dst_image,float  * src_image,int const w,int const h, int const c, int const num_channels,float const value,float *contrast)
+{
+    add_channels_cu<float>(dst_image,src_image, w, h, c,num_channels,value);
+    //compare1(dst_image,contrast,w*c,h, true);
+    return  0;
 }
